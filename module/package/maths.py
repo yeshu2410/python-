97fd60af7fa54aaf5395f60b94bca174bf59507f
yeshu2410 <myeshu2410@gmail.com>
2024-12-12 18:47:47 -0800

    Initial Commit

---
+++ module/package/maths.py
@@ -0,0 +1,4 @@
+def addition(a,b):
+    return a+b
+def substractions(a,b):
+    return a-b
---
+++ module/testing.py
@@ -0,0 +1,5 @@
+from package.maths import *
+from package.subpackages.mult import multiplication
+print(addition(1,2))
+print(substractions(4,3))
+print(multiplication(2,3))
